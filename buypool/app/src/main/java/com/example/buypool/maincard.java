7b36c94c75530db34a3b6534f3f70f2782d35abc
jiaru01 <jiaru.he@ucdconnect.ie>
2019-11-02 20:26:41 +0000

    try cardview

--- buypool/app/src/main/java/com/example/buypool/maincard.java
+++ buypool/app/src/main/java/com/example/buypool/maincard.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class cardpage extends AppCompatActivity {
+public class maincard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cardpage);
+        setContentView(R.layout.activity_maincard);
     }
 }
