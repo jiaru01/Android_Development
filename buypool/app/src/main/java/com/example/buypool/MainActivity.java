0e9351349bc36c5777c6ca8827f5ed78511779ab
jensenjay <46137283+jensenjay@users.noreply.github.com>
2019-11-06 15:50:15 +0000

    complete the login and sign up activity

--- buypool/app/src/main/java/com/example/buypool/MainActivity.java
+++ buypool/app/src/main/java/com/example/buypool/MainActivity.java
@@ -1,22 +1,24 @@
 package com.example.buypool;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
 
+import android.content.ContentValues;
 import android.content.Intent;
-import android.graphics.PorterDuff;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Toast;
-import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnLogin, btnSignup;
+    private Button btnLogin , btnSignup, getBtnLogin;
+    private EditText email, password;
+    private CheckBox remember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,41 +26,94 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnLogin = findViewById(R.id.btnLogin);
         btnSignup = findViewById(R.id.btnSignup);
+        getBtnLogin = findViewById(R.id.button3);
+        email = (EditText) findViewById(R.id.EnterEmail);
+        password = (EditText) findViewById(R.id.Password);
+        remember = (CheckBox) findViewById(R.id.checkBox);
 
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                OpenSignUpActivity();
+                openSignUpActivity();
             }
         });
-
-
-//        This should be deleted later , used for testing the tool bar
-        Toolbar toolbar = findViewById(R.id.profile_toolbar);
-        setSupportActionBar(toolbar);
-        TextView textView = toolbar.findViewById(R.id.toolbar_title);
-        textView.setText("String");
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-
-//        Change color of the overflow 'The three dot' on Tool bar
-        toolbar.getOverflowIcon().setColorFilter(ContextCompat.getColor(this, R.color.white), PorterDuff.Mode.SRC_ATOP);
-//        This should be deleted later , used for testing the tool bar
-
+        getBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                storeUser();
+            }
+        });
+        getUser();
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openLoginActivity();
+//            }
+//        });
     }
-
-//    here delete for toolbar
-    public boolean onCreateOptionsMenu(Menu menu) {
-//        push menu icons onto the toolbars
-        getMenuInflater().inflate(R.menu.buypooldisplaypage, menu);
-        return true;
-    }
-//    here delete for toolbar
-
-
-    public void OpenSignUpActivity() {
+    public void openSignUpActivity() {
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
     }
+//    public void openLoginActivity() {
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//    }
+    public void getUser(){
+        LocalDatabase helper = new LocalDatabase(getApplicationContext(), "User", null, 1);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        Cursor users = db.query("userslocal", null, null, null, null, null, null);
+            if (users.moveToLast()){
+                while (true){
+                    int checked = users.getInt(3);
+                    if (checked == 1) {
+                        this.remember.setChecked(true);
+                        String email = users.getString(1);
+                        String password = users.getString(2);
+                        this.email.setText(email);
+                        this.password.setText(password);
+                        break;
+                    }{
+                        if (!users.moveToPrevious())
+                            break;
+                    }
+                }
+            }
 
+    }
+
+    public void storeUser(){
+        LocalDatabase helper = new LocalDatabase(getApplicationContext(), "User", null, 1);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        String email = this.email.getText().toString();
+        String password = this.password.getText().toString();
+        Cursor usersremote = db.query("usersremote", null, "email=? and password=?", new String[]{email, password}, null, null, null);
+        if (usersremote.moveToLast()){
+            Toast.makeText(getApplicationContext(), "ok", Toast.LENGTH_LONG).show();
+            if (this.remember.isChecked()){
+                Cursor user = db.query("userslocal", null, "email=?", new String[]{email}, null, null, null);
+                if (user.moveToLast()){
+                    Cursor isSame = db.query("userslocal", null, "email = ? and password=?", new String[]{email,password}, null, null, null);
+                    if(!isSame.moveToLast()){
+                        ContentValues content = new ContentValues();
+                        content.put("password", password);
+                        db.update("userslocal", content, "email = ？",new String[]{email});
+                    }
+                }else {
+                    ContentValues contenValuses = new ContentValues();
+                    contenValuses.put("email", email);
+                    contenValuses.put("password", password);
+                    contenValuses.put("is_remember", 1);
+                    db.insert("userslocal", null, contenValuses);
+                }
+
+            }
+        }else {
+            Toast.makeText(getApplicationContext(), "Username or Password Wrong!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+
+    }
 }
