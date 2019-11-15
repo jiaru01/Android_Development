48e8521bb1864729971c4123183986887539b5cf
Deyan Chen <>
2019-11-15 00:21:43 +0000

    Put card and onto collected car

--- buypool/app/src/main/java/com/example/buypool/CardCollectionActivity.java
+++ buypool/app/src/main/java/com/example/buypool/CardCollectionActivity.java
@@ -3,6 +3,8 @@ package com.example.buypool;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.graphics.PorterDuff;
@@ -10,6 +12,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CardCollectionActivity extends AppCompatActivity {
 
@@ -31,8 +35,83 @@ public class CardCollectionActivity extends AppCompatActivity {
 //Sets tool bar ends here
 
 
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewCardCollection);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));//create a recycling view in a linear layout
+        MyAdapter Myadapter = new MyAdapter( this,getMyList());
+        recyclerView.setAdapter(Myadapter);
 
 
+
+
+    }
+    //modifiy the return data as and set model
+    private ArrayList<Model> getMyList(){
+
+        ArrayList<Model> models = new ArrayList<>();
+
+        Model m = new Model();//We built this model
+        m.setTitle("here is card collection baby ");
+        m.setDesription("This is newsfeed descripstion");
+        m.setImg(R.drawable.male);
+        m.setDate("14/11/2019");
+        m.setAddress("road");
+        m.setUserNameOnCard("Jack");
+        models.add(m);
+
+        m = new Model();//We built this model
+        m.setTitle("New 44Feeds");
+        m.setDesription("This is newsfeed descripstiLorem ipsum dolor sit " +
+                "\n amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt uLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt uLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt uon");
+        m.setImg(R.drawable.male);
+        m.setDate("15/11/2019");
+        m.setAddress("Grove");
+        m.setUserNameOnCard("morephy");
+        models.add(m);
+
+
+        m = new Model();//We built this model
+        m.setTitle("New Feeds");
+        m.setDesription("This is newsfeed descripstion");
+        m.setImg(R.drawable.male);
+        m.setDate("16/11/2019");
+        m.setAddress("Grove");
+        m.setUserNameOnCard("wahhha");
+
+        models.add(m);
+
+        m = new Model();//We built this model
+        m.setTitle("New Feeds");
+        m.setDesription("This is newsfeed descripstion");
+        m.setImg(R.drawable.male);
+        m.setDate("15/11/2019");
+        m.setAddress("Grove");
+        m.setUserNameOnCard("wahhha");
+
+        models.add(m);
+
+
+        m = new Model();//We built this model
+        m.setTitle("New Feeds");
+        m.setDesription("This is newsfeed descripstion");
+        m.setImg(R.drawable.male);
+        m.setDate("15/11/2019");
+        m.setUserNameOnCard("wahhha");
+
+        m.setAddress("Grove");
+        models.add(m);
+
+        m = new Model();//We built this model
+        m.setTitle("New Feeds");
+        m.setDesription("This is newsfeed descripstion");
+        m.setImg(R.drawable.male);
+        m.setDate("15/11/2019");
+        m.setUserNameOnCard("wahhha");
+
+        m.setAddress("Grove");
+        models.add(m);
+
+
+        return models;
     }
 
 
